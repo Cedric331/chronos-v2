81b4127dc22614e06664191f060919fae90b651e
Cédric <limacedric@hotmail.fr>
2023-05-18 17:32:27 +0200

    Modif modal generation planning

--- resources/js/Pages/Team/Modal/ModalPlanning.vue
+++ resources/js/Pages/Team/Modal/ModalPlanning.vue
@@ -11,11 +11,11 @@
                 <div class="w-full md:flex mt-8">
 
                     <div class="mt-8 max-w-sm md:mt-0 md:ml-10 md:w-1/3">
-                        <div @click="tabs = 1" class="relative flex pb-12">
+                        <div class="relative flex pb-12">
                             <div class="absolute inset-0 flex h-full w-10 items-center justify-center">
                                 <div class="pointer-events-none h-full w-1 bg-gray-200"></div>
                             </div>
-                            <div class="relative z-10 inline-flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-blue-500 text-white">
+                            <div @click="tabs = 1" class="relative z-10 inline-flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full text-white cursor-pointer" :class="[dateStart && dateEnd ? 'bg-green-600' : 'bg-blue-500']">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                                 </svg>
@@ -55,7 +55,6 @@
                         <div class="mx-auto">
                             <div class="w-full flex flex-col bg-white rounded-lg">
                                 <h1 class="font-semibold text-center tracking-wide mb-2">Sélection des dates</h1>
-
                                 <div class="flex flex-col justify-between">
                                     <label for="start">Semaine de début:</label>
                                     <input type="week" id="start" name="trip-start" class="w-full"
@@ -66,6 +65,7 @@
                                            v-model="dateEnd"
                                            :min="dateLimitStart" :max="dateLimitEnd">
                                 </div>
+                                <p class="text-xs text-gray-500 mt-6 w-[300px]">Vous devez indiquer les dates de début et de fin du planning qui sera généré.</p>
                             </div>
                         </div>
                     </div>
@@ -89,8 +89,19 @@
                             </div>
                         </div>
                     </div>
-
                 </div>
+
+
+                <div class="flex w-full mb-5 mr-5" :class="tabJustify">
+                    <svg @click="this.tabs--" v-if="tabs !== 1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+
+                    <svg @click="this.tabs++" v-if="tabs !== 3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </div>
+
             </div>
         </section>
 
@@ -116,9 +127,49 @@ export default {
         team: Object,
         showPlanning: Boolean
     },
+    computed: {
+        dateLimitStart() {
+            const now = new Date();
+            const day = now.getDay();
+            const diff = now.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
+            const monday = new Date(now.setDate(diff));
+            const weekNumber = this.getWeekNumber(monday);
+            return `${monday.getFullYear()}-W${weekNumber.toString().padStart(2, '0')}`;
+        },
+        dateLimitEnd() {
+            const now = new Date();
+            now.setFullYear(now.getFullYear() + 1);
+            now.setMonth(now.getMonth() + 1);
+            const weekNumber = this.getWeekNumber(now);
+            return `${now.getFullYear()}-W${weekNumber.toString().padStart(2, '0')}`;
+        },
+    },
+    watch: {
+        tabs() {
+            if (this.tabs === 2) {
+                this.tabJustify = 'justify-between';
+            } else if (this.tabs === 1) {
+                this.tabJustify = 'justify-end';
+            } else if (this.tabs === 3) {
+                this.tabJustify = 'justify-start';
+            }
+        }
+    },
     data () {
         return {
-            tabs: 1
+            tabs: 1,
+            tabJustify: 'justify-end',
+            dateStart: null,
+            dateEnd: null,
+        }
+    },
+    methods: {
+        getWeekNumber(d) {
+            d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+            d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+            const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+            const weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+            return weekNo;
         }
     }
 }
