ff92d62def8dec3abf4fd266c5d40912c0ac7e18
Cédric <limacedric@hotmail.fr>
2023-08-27 21:08:10 +0200

    Add middleware check cnnection

---
+++ app/Http/Middleware/HasConnection.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HasConnection
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$this->hasInternetConnection()) {
+            return response()->json([
+                'message' => 'No internet connection'
+            ], Response::HTTP_SERVICE_UNAVAILABLE);
+        }
+
+        return $next($request);
+    }
+
+    private function hasInternetConnection()
+    {
+        // Vous pouvez, par exemple, utiliser un ping ou une autre méthode pour vérifier la connexion
+        $connected = @fsockopen("www.google.com", 80);
+        if ($connected){
+            fclose($connected);
+            return true;
+        }
+        return false;
+    }
+
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,7 @@ class UserFactory extends Factory
             'email' => fake()->email(),
             'email_verified_at' => now(),
             'team_id' => 1,
+            'company_id' => 1,
             'account_active' => true,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
