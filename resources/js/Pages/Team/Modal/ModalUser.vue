5dbaa021534e743dba3f36fa926e2cb27f48ce54
Cédric <limacedric@hotmail.fr>
2023-09-04 21:06:29 +0200

    fix

--- resources/js/Pages/Team/Modal/ModalUser.vue
+++ resources/js/Pages/Team/Modal/ModalUser.vue
@@ -12,6 +12,7 @@
                 </div>
             </div>
 
+
             <div class="mb-5">
                 <div class="relative">
                     <input v-model="item.email" type="text" id="email" class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
