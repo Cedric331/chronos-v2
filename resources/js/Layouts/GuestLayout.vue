3efb59929242b3cfacf349e62e0c1031a3935687
Cédric <limacedric@hotmail.fr>
2023-11-01 14:01:30 +0100

    update background

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -3,10 +3,21 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center bg-gray-100 dark:bg-gray-900" style="background-image: url('/images/background_login.jpg')">
+    <div id="background" class="min-h-screen flex flex-col sm:justify-center items-center bg-gray-100 dark:bg-gray-900">
 
         <div class="w-full dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
             <slot />
         </div>
     </div>
 </template>
+
+<style>
+#background {
+    background-image: url('/images/background_login.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 100vh;
+    width: 100%;
+}
+</style>
