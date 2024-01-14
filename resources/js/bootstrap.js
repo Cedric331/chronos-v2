21ff7bf52b93d753c1e7468d865bd7195b4b3cd7
Cédric <limacedric@hotmail.fr>
2024-01-14 16:25:49 +0100

    fix stash

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,7 +12,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.headers.common['X-XSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
 
-
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
