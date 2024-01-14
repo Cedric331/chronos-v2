c1b1a0f744191103e8bfdaf585770676e2bbca63
Cédric <limacedric@hotmail.fr>
2024-01-14 21:07:37 +0100

    fix

--- resources/js/Pages/Calendar/Partials/ViewWeekTeam.vue
+++ resources/js/Pages/Calendar/Partials/ViewWeekTeam.vue
@@ -29,14 +29,14 @@
                 <div class="flex items-center">
 
                     <div class="flex flex-col justify-center mx-auto" v-if="planning.debut_journee">
-                        <p class="text-md font-bold text-center">{{ planning.type_day }} - {{ planning.rotation.name }}</p>
+                        <p class="text-md font-bold text-center">{{ planning.type_day }} {{ planning.rotation ? ' - ' + planning.rotation.name : '' }}</p>
                         <p v-if="planning.debut_journee" class="text-md">Début de Journée : <strong>{{ planning.debut_journee }}</strong></p>
                         <p v-if="planning.debut_pause" class="text-md">Début de Pause : <strong>{{ planning.debut_pause }}</strong></p>
                         <p v-if="planning.fin_pause" class="text-md">Fin de Pause : <strong>{{ planning.fin_pause }}</strong></p>
                         <p v-if="planning.fin_journee" class="text-md">Fin de Journée : <strong>{{ planning.fin_journee }}</strong></p>
                     </div>
                     <div v-else class="mx-auto">
-                        <p class="text-md font-bold text-center">{{ planning.type_day }} - {{ planning.rotation.name }}</p>
+                        <p class="text-md font-bold text-center">{{ planning.type_day }} {{ planning.rotation ? ' - ' + planning.rotation.name : '' }}</p>
                     </div>
                 </div>
             </td>
